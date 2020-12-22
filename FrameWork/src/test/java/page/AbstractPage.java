f7cd3118781377cb353098a793c57c6ab698c6da
Моргунов Владислав <46040268+Nachalas@users.noreply.github.com>
2020-12-23 00:04:42 +0300

    Update AbstractPage.java

--- FrameWork/src/test/java/page/AbstractPage.java
+++ FrameWork/src/test/java/page/AbstractPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.Driver;
-
 public abstract class AbstractPage
 {
 	protected WebDriver driver;
